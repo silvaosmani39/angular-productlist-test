602ca31d771d27c06d7586631dd237b5dc6f4d0b
Osmani Silva <159924319+silvaosmani39@users.noreply.github.com>
2024-06-13 10:19:37 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF5507",
+        text: "#262626",
+        gray: "#999"
+      },
+    },
+  },
+  plugins: [],
+};
